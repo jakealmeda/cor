51525fd46ffb8686f1f6754abf4f69babacb1728
Mark Corpuz <social@markcorpuz.com>
2017-06-26 11:31:16 -0700

    Making major changes to functions file declarations
    This is the start of changes I’m starting to make on the COR child theme moving towards upgrading the existing child theme to a new Genesis format.

--- functions.php
+++ functions.php
@@ -1,48 +1,176 @@
 <?php
-//* Start the engine
+/**
+ * Genesis Sample.
+ *
+ * This file adds functions to the COR Theme.
+ *
+ * @package COR
+ * @author  Mark Corpuz
+ * @license GPL-2.0+
+ * @link    http://markcorpuz.com/
+ */
+
+// Start the engine.
 include_once( get_template_directory() . '/lib/init.php' );
 
+// Setup Theme.
+include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+
+// Set Localization (do not remove).
+add_action( 'after_setup_theme', 'genesis_sample_localization_setup' );
+function genesis_sample_localization_setup(){
+	load_child_theme_textdomain( 'base-starter', get_stylesheet_directory() . '/languages' );
+}
+
+// Add the helper functions.
+include_once( get_stylesheet_directory() . '/lib/helper-functions.php' );
+
+// Add Image upload and Color select to WordPress Theme Customizer.
+require_once( get_stylesheet_directory() . '/lib/customize.php' );
+
+// Include Customizer CSS.
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+// Add WooCommerce support.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php' );
+
+// Add the required WooCommerce styles and Customizer CSS.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php' );
+
+// Add the Genesis Connect WooCommerce notice.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php' );
+
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Basic SASS' );
-define( 'CHILD_THEME_URL', 'http://www.smarterwebpackages.com/' );
-define( 'CHILD_THEME_VERSION', '2.1.2.0' );
+define( 'CHILD_THEME_NAME', 'COR' );
+define( 'CHILD_THEME_URL', 'http://smarterwebpackages.com' );
+define( 'CHILD_THEME_VERSION', '2.3.0.1' );
 
-//* Add support for structural wraps
-add_theme_support( 'genesis-structural-wraps', array(
-	'header', 'nav', 'subnav', 'site-inner', 'footer-widgets', 'footer'
-) );
+// Enqueue Scripts and Styles.
+add_action( 'wp_enqueue_scripts', 'basestarter_enqueue_scripts_styles' );
+function basestarter_enqueue_scripts_styles() {
 
-//* Add HTML5 markup structure
-add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+	wp_enqueue_style( 'basestarter-fonts', '//fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Roboto+Condensed:300,400,700|Lato:100,300,400,700,900', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'dashicons' );
 
-//* Add viewport meta tag for mobile browsers
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	wp_enqueue_script( 'basestarter-responsive-menu', get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js", array( 'jquery' ), CHILD_THEME_VERSION, true );
+	wp_localize_script(
+		'basestarter-responsive-menu',
+		'genesis_responsive_menu',
+		basestarter_responsive_menu_settings()
+	);
+
+}
+
+// Define our responsive menu settings.
+function basestarter_responsive_menu_settings() {
+
+	$settings = array(
+		'mainMenu'          => __( 'Menu', 'base-starter' ),
+		'menuIconClass'     => 'dashicons-before dashicons-menu',
+		'subMenu'           => __( 'Submenu', 'base-starter' ),
+		'subMenuIconsClass' => 'dashicons-before dashicons-arrow-down-alt2',
+		'menuClasses'       => array(
+			'combine' => array(
+				'.nav-primary',
+				'.nav-header',
+			),
+			'others'  => array(),
+		),
+	);
+
+	return $settings;
+
+}
+
+// Add HTML5 markup structure.
+add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+
+// Add Accessibility support.
+add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+
+// Add viewport meta tag for mobile browsers.
 add_theme_support( 'genesis-responsive-viewport' );
 
-//* Add support for custom background
+// Add support for custom header.
+add_theme_support( 'custom-header', array(
+	'width'           => 600,
+	'height'          => 160,
+	'header-selector' => '.site-title a',
+	'header-text'     => false,
+	'flex-height'     => true,
+) );
+
+// Add support for custom background.
 add_theme_support( 'custom-background' );
 
-//* Add support for 3-column footer widgets
+// Add support for after entry widget.
+add_theme_support( 'genesis-after-entry-widget-area' );
+
+// Add support for 3-column footer widgets.
 add_theme_support( 'genesis-footer-widgets', 3 );
 
-//* Add support for after entry widget
-//add_theme_support( 'genesis-after-entry-widget-area' );
+// Add Image Sizes.
+add_image_size( 'featured-image', 720, 405, TRUE );
 
-//* Reposition Secondary Sidebar before CSW
-remove_action('genesis_sidebar_alt','genesis_do_sidebar_alt');
-//add_action('genesis_before_content_sidebar_wrap', 'genesis_do_sidebar_alt');
+// Rename primary and secondary navigation menus.
+add_theme_support( 'genesis-menus', array( 'primary' => __( 'Top Menu', 'genesis-sample' ), 'secondary' => __( 'Header Menu', 'genesis-sample' ) ) );
 
-//* Remove the site footer
-remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
-remove_action( 'genesis_footer', 'genesis_do_footer' );
-remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
+// Reposition the primary navigation menu.
+//remove_action( 'genesis_after_header', 'genesis_do_nav' );
+//add_action( 'genesis_header_right', 'genesis_do_subnav', 5 );
 
-//* Enqueue scripts and styles for responsive menu
-add_action( 'wp_enqueue_scripts', 'js_menu' );
-function js_menu() {
-	//wp_enqueue_script( 'menu-responsive', get_bloginfo( 'stylesheet_directory' ) . '/js/menu.js', array( 'jquery' ), '1.0.0' );
-	wp_enqueue_script( 'menu-responsive', get_bloginfo( 'stylesheet_directory' ) . '/js/menu.js', array( 'jquery' ), '1.0.0', true );
-	//wp_enqueue_style( 'dashicons' );
+// Reposition the secondary navigation menu.
+//remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+//add_action( 'genesis_before_header', 'genesis_do_subnav', 5 );
+
+// Reduce the secondary navigation menu to one level depth.
+add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+function genesis_sample_secondary_menu_args( $args ) {
+
+	if ( 'secondary' != $args['theme_location'] ) {
+		return $args;
+	}
+
+	$args['depth'] = 1;
+
+	return $args;
+
 }
+
+// Modify size of the Gravatar in the author box.
+add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+function genesis_sample_author_box_gravatar( $size ) {
+	return 90;
+}
+
+// Modify size of the Gravatar in the entry comments.
+add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+function genesis_sample_comments_gravatar( $args ) {
+
+	$args['avatar_size'] = 60;
+
+	return $args;
+
+}
+
+//* Customize the entire footer | functions-sitefooter
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+add_action( 'genesis_footer', 'spt_sitefooter' );
+function spt_sitefooter() {
+	?>
+	<div class="siteby"><a href="http://smarterwebpackages.com/">SmarterWebPackages.com</a></div>
+	<div class="copyright">Copyright © 2016 UnderstandingRelatonships.com | All Rights Reserved | <a href="http://www.understandingrelationships.com/privacy-policy/">Privacy Policy</a><br>
+	The Corey Wayne Companies | Orlando, FL USA</div>
+	<?php
+}
+
+
+
+
+
+
+
 
 //* Remove p tags around images
 function filter_ptags_on_images($content){
@@ -66,21 +194,6 @@ add_filter( 'genesis_post_meta', 'sp_post_meta_filter' );
 		return $post_meta;
 	}
 
-//* Customize the entire footer | functions-sitefooter
-remove_action( 'genesis_footer', 'genesis_do_footer' );
-add_action( 'genesis_after_footer', 'spt_sitefooter', 5 );
-	function spt_sitefooter() {
-		?>
-		<div class="module-credits">
-			<div class="module-wrap">
-				<div class="siteby"><a href="http://smarterwebpackages.com/">SmarterWebPackages.com</a></div>
-				<div class="copyright">Copyright © 2016 UnderstandingRelatonships.com | All Rights Reserved | <a href="http://www.understandingrelationships.com/privacy-policy/">Privacy Policy</a><br>
-				The Corey Wayne Companies | Orlando, FL USA</div>
-			</div>
-		</div>
-		<?php
-	}
-
 //* Apply widget area - beforecontent
 add_action( 'genesis_before_content', 'base226_beforecontent' );
 	function base226_beforecontent() {
@@ -91,24 +204,6 @@ add_action( 'genesis_before_content', 'base226_beforecontent' );
 			) );
 		} else {}
 	}
-
-//* Apply widget area - minisidebar
-/*
-add_action( 'genesis_before_content_sidebar_wrap', 'base227_minisidebar' );
-	function base227_minisidebar() {
-		if (is_front_page()) {
-			genesis_widget_area( 'minisidebar', array(
-				'before' => '<div class="home minisidebar widget-area"><div class="areawrap">',
-				'after' => '</div></div>',
-			) );
-		} else {
-			genesis_widget_area( 'minisidebar', array(
-				'before' => '<div class="minisidebar widget-area"><div class="areawrap">',
-				'after' => '</div></div>',
-			) );
-		}
-	}
-*/
 
 //* Apply widget area - cswbefore
 add_action( 'genesis_before_content_sidebar_wrap', 'base226_cswbefore' );
